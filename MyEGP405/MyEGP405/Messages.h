45ce38d9d119335483bc93169b2b96db5a5c30c1
Justin Mulkin <litbass12@gmail.com>
2017-09-21 04:40:06 -0400

    Added standard headers to the files

--- MyEGP405/MyEGP405/Messages.h
+++ MyEGP405/MyEGP405/Messages.h
@@ -1,3 +1,18 @@
+// Brian Baron		0974390
+// Colin Brady		0979605
+// Justin Mulkin	0952465
+//
+// EGP 405-02	Project 1	2017/09/17 (YYYY/MM/DD)
+//
+//
+//--------------------------------------------------------------------------------------------------------------------------------
+//		 We certify that this work is entirely our own.The assessor of this project may reproduce this project
+//		 and provide copies to other academic staff, and/or communicate a copy of this project to a plagiarism-checking service,
+//		 which may retain a copy of the project on its database
+//--------------------------------------------------------------------------------------------------------------------------------
+//
+// Repository Link: https://github.com/tgpx31/baron-egp-405
+//
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
