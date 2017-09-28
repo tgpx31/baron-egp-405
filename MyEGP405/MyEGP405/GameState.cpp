df03426e35a41c754a856648d0046a55c2cf2384
Justin_Mulkin <justin.mulkin@mymail.champlain.edu>
2017-09-28 11:56:20 -0400

    Fixed certificates of authenticity

--- MyEGP405/MyEGP405/GameState.cpp
+++ MyEGP405/MyEGP405/GameState.cpp
@@ -1,3 +1,17 @@
+// Brian Baron		0974390
+// Justin Mulkin	0952465
+//
+// EGP 405-02	Lab 2	2017/09/25	(YYYY/MM/DD)
+//
+//
+//--------------------------------------------------------------------------------------------------------------------------------
+//		 We certify that this work is entirely our own.The assessor of this project may reproduce this project
+//		 and provide copies to other academic staff, and/or communicate a copy of this project to a plagiarism-checking service,
+//		 which may retain a copy of the project on its database
+//--------------------------------------------------------------------------------------------------------------------------------
+//
+// Repository Link: https://github.com/tgpx31/baron-egp-405
+//
 #include "GameState.h"
 
 void GameState::init(State * prev = nullptr, State ** currentState = nullptr)
